bd3fe93cf85714e6ea34802d7b7be1473b2d6d26
cameronchandra1 <31973106+cameronchandra1@users.noreply.github.com>
2017-10-30 17:55:39 -0400

    Delete twitter_info.py

--- twitter_info.py
+++
@@ -1,7 +0,0 @@
-
-consumer_key = "Sg3Vq2pfZiqD1Dz3tQlthpVjc"
-consumer_secret = "mkG0nwQw3Jy62iGJIE81fYXQS1hhE4EGek2eUpyyzb0dym9v1M"
-access_token = "924710307519492096-A7QjyGERUujXCgIHz7gHSVvb5cp9d6p"
-access_token_secret = "37MeD53wAgGrZPdY7qXSxtX1vubYWdQsPxXbi4dxQM0zM"
-
-
